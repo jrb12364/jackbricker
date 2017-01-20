82368164bb39a97b50ec60151feb29398016731d
BuildTools <unconfigured@null.spigotmc.org>
2017-01-20 12:21:53 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+		starClock = 3; // Set a lower number for a higher spawn frequency
+		fadeSpeed = 300; // Set a higher number for a longer delay between elements
+
+		i = 1;
+		setInterval(function() {
+				if (Math.floor(Math.random() * starClock) == 0 && document.hasFocus()) {
+						height = $(window).height() * Math.random();
+						speed = (Math.random() + 1) * 15;
+						opacity = (Math.random() / 2) + 0.5;
+						$('.bg').append('<img src="img/star.png" class="star" style="top: ' + height + 'px; opacity: ' + opacity + '; -webkit-animation: star ' + speed + 's linear; -moz-animation: star ' + speed + 's linear; animation: star ' + speed + 's linear;">');
+						i++;
+				}
+		}, 100);
+});
+$(window).load(function() {
+		$('body').addClass('fade-body');
+		j = 1;
+		fade = setInterval(function() {
+				if (j <= $('.fade').length) {
+						$('.fade:nth-child(' + j + ')').addClass('faded');
+				} else {
+						clearInterval(fade);
+				}
+				j++;
+		}, fadeSpeed);
+});
